840defb930a6b7db67eef58af5ecfeb3c72d68d2
Anil G <anil.gangadharaiah@neudesic.com>
2017-12-26 21:32:29 +0530

    adding sample form bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=nmshelpdeskpoc1-Web-Deploy.pubxml -p:Password=9Fv1vnoqwEhFiz2kg1xE8qjvGmAXRQwGQJtGcr6jTipfJnFhsCdBqEd3bXNt
+
